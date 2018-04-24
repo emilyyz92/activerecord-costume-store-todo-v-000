38e9140b7c9c3eda6b9600dffd24d16aceac6263
Emily Zhao <tong.zhao.92@gmail.com>
2018-04-24 23:09:33 +0000

    automatically backed up by learn

--- db/schema.rb
+++ db/schema.rb
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 3) do
     t.string "theme"
     t.float "price"
     t.boolean "family_friendly"
-    t.datetime "opening_time"
-    t.datetime "closing_time"
+    t.datetime "opening_date"
+    t.datetime "closing_date"
     t.string "description"
   end
 
